9d380b825f1082cbf0617ba933781daec4717cff
leeem <hs162a2@gmail.com>
2018-11-08 15:32:12 -0500

    mk IIIa

--- src/capstone_3_runs_on_robot.py
+++ src/capstone_3_runs_on_robot.py
@@ -7,11 +7,8 @@ Also: responds to Beacon button-presses by beeping, speaking.
 This module runs on the ROBOT.
 It uses MQTT to RECEIVE information from a program running on the LAPTOP.
 
-Authors:  David Mutchler, his colleagues, and PUT_YOUR_NAME_HERE.
+Authors:  David Mutchler, his colleagues, and Eric Lee.
 """
-# ------------------------------------------------------------------------------
-# TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.  Then delete this TODO.
-# ------------------------------------------------------------------------------
 
 # ------------------------------------------------------------------------------
 # TODO: 2. With your instructor, review the "big picture" of laptop-robot
@@ -29,12 +26,16 @@ def main():
     # --------------------------------------------------------------------------
     # TODO: 3. Construct a Snatch3rRobot.  Test.  When OK, delete this TODO.
     # --------------------------------------------------------------------------
+    robot = rb.Snatch3rRobot()
 
     # --------------------------------------------------------------------------
     # TODO: 4. Add code that constructs a   com.MqttClient   that will
     # TODO:    be used to receive commands sent by the laptop.
     # TODO:    Connect it to this robot.  Test.  When OK, delete this TODO.
     # --------------------------------------------------------------------------
+    rc = RemoteControlEtc(robot)
+    mqtt_client = com.MqttClient(rc)
+    mqtt_client.connect_to_pc()
 
     # --------------------------------------------------------------------------
     # TODO: 5. Add a class for your "delegate" object that will handle messages
@@ -58,5 +59,19 @@ def main():
         # ----------------------------------------------------------------------
         time.sleep(0.01)  # For the delegate to do its work
 
+
+class RemoteControlEtc(object):
+    def __init__(self, robot):
+        """
+        stores the robot.
+        :type robot: rb.Snatch3rRobot
+        """
+        self.robot = robot
+
+    def go_forward(self, speed_string):
+        """makes the robot go forward at a given speed"""
+        print('telling the robot to start moving at', speed_string)
+        speed = int(speed_string)
+        self.robot.drive_system.start_moving(speed, speed)
 
 main()
